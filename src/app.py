6337243858e8bbbb3885b8fff438b08882eb6514
Praveen Govindaraj <praveengovi.analytics@gmail.com>
2023-09-08 11:01:17 +0800

    Merge branch 'dev' custom_model

--- src/app.py
+++ src/app.py
@@ -6,7 +6,11 @@ from langchain.chains import ConversationalRetrievalChain
 from langchain.document_loaders.csv_loader import CSVLoader
 from langchain.vectorstores import FAISS
 import tempfile
-impo
+<<<<<<< HEAD
+
+=======
+import os
+>>>>>>> 9a2291677f91277dd362ce69d08c9ff7547d97be
 
 #user_api_key = st.sidebar.text_input(
 #    label="#### Your OpenAI API key 👇",
