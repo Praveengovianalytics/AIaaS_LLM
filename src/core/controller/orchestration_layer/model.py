0ca07e16bb28d063b080e11489713c4925839f1c
Chen ZhuoFan <chenzhuofan.chen@singtel.com>
2023-11-14 10:09:26 +0800

    AIS-16: Prompt Change for No File

--- src/core/controller/orchestration_layer/model.py
+++ src/core/controller/orchestration_layer/model.py
@@ -101,7 +101,7 @@ class LLM:
                         }
                     )
                 else:
-                    chat='\n'.join([f"User: {index[0]} \n Assistant: {index[1]}" for index,i in enumerate(chat_history)])
+                    chat='\n'.join([f"User: {i[0]} \n Assistant: {i[1]}" for index,i in enumerate(chat_history)])
                     prompt=f'''
                     You are a helpful chatbot. I will provide you the question and the chat history . Answer user's question according to needs.
                     
